48603bc17a78c0318ae5f1ab052f52a0b1d74b12
Thanu23 <48243155+Thanu23@users.noreply.github.com>
2021-08-27 19:09:47 +0530

    Add files via upload

---
+++ Python/Activities/hello.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+ 
+print(type(x)) # Output: <class 'int'>
+print(type(y)) # Output: <class 'float'>
+print(type(z)) # Output: <class 'complex'>
+
+	
+a = "Hello"
+print(a) # Output: "Hello"
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
